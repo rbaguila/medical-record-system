c14efe631808a2e76b0192c227ed3d75a1357f0f
Roinand Aguila <justinepaulguiao@gmail.com>
2017-05-30 16:52:16 +0800

    Added test file

--- server/test/test-server.js
+++ server/test/test-server.js
@@ -2,8 +2,14 @@ var assert = require('assert');
 var supertest = require('supertest');
 var express = require('express');
 
+describe('Test server responses', function() {
+    var url = 'http://localhost:3001/api/';
 
+    it('Homepage responds with "API initialized!"', function(done) {
+        request(url)
+            .get('/')
+            .expect('API Initialized!')
+            .expect(200, done);
+    });
 
-
-
-
+});
