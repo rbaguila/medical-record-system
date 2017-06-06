bbcba6b5eba7cda1ac725b0eabef006cc7eeb26d
Roinand Aguila <rbaguila1@up.edu.ph>
2017-06-07 00:01:00 +0800

    Fixed hackernews clone in App.js

--- src/App.js
+++ src/App.js
@@ -11,82 +11,70 @@ const sampleAPI = `http://localhost:3001/api/users/`;
 console.log(url);
 console.log(sampleAPI);
 
-function isSearched(searchTerm){
-  return function(list){
+function isSearched(searchTerm) {
+  return function(item) {
     return !searchTerm ||
-      list.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      list.author.toLowerCase().includes(searchTerm.toLowerCase());
+      item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.author.toLowerCase().includes(searchTerm.toLowerCase());
   }
 }
 
-
 class App extends Component {
-  
-  constructor(props){
+  constructor(props) {
     super(props);
-
-    //Sets the states for this app
     this.state = {
       result: null,
       searchTerm: DEFAULT_QUERY,
     };
-
     this.setSearchTopstories = this.setSearchTopstories.bind(this);
     this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
-
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
-  
-  setSearchTopstories(result){
-    this.setState ({ result });
+  setSearchTopstories(result) {
+    this.setState({ result });
   }
 
-  fetchSearchTopstories(searchTerm){
+  fetchSearchTopstories(searchTerm) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopstories(result));
+    .then(response => response.json())
+    .then(result => this.setSearchTopstories(result));
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const { searchTerm } = this.state;
     this.fetchSearchTopstories(searchTerm);
   }
 
-  //For Search
-  onSearchChange(event){
-    this.setState({ searchTerm: event.target.value });
-  }
-
-  onDismiss(id){
+  onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({
-      result: Object.assign({}, this.state.result, {hits: updatedHits})
+      result: { ...this.state.result, hits: updatedHits }
     });
   }
 
-  render() {
-    const {searchTerm, result } = this.state
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
-    if(!result){
-      return null;
-    }
+  render() {
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
       <div className="page">
-          <div className="interactions">
-
-            <Search 
-              onChange = {this.onSearchChange}
-            >
-
-             Search
-            </Search>
-          </div>
-
+        <div className="interactions">
+          <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+          >
+          Search
+          </Search>
+        </div>
         <Table 
-          list = {result.hits}
-          pattern = {searchTerm}
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
         />
 
       </div>
@@ -94,78 +82,47 @@ class App extends Component {
   }
 }
 
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-//Class Search
-class Search extends Component{
-  render(){
-    const {value, onChange, children} = this.props;
-    /*
-      This can also be done by:
-
-      const value = this.props.value;
-      const onChange = this.props.onChange;
-    */
-    
-    return(
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
+const Button = ({onClick, className = '', children}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+  {children}
+  </button>
 
 
 
-//Class Table
-class Table extends Component{
-  render(){
-    const {list, pattern} = this.props;
-    return(
-      <div className="table">
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID} className="table-row">
-            <span>
-              <a href={item.url}> {item.title} </a>
-            </span>
+const Table = ({ list, pattern, onDismiss}) =>
+  <div className="table">
+      { list.filter(isSearched(pattern)).map(item =>
+        <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%' }}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={{ width: '30%' }}>{item.author}</span>
+          <span style={{ width: '10%' }}>{item.num_comments}</span>
+          <span style={{ width: '10%' }}>{item.points}</span>
+          <span style={{ width: '10%' }}>
+          <Button 
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+          </span>
+        </div>
+      )}
+    </div>
 
-            <span> {item.author} </span>
-            <span> {item.num_comments} </span>
-            <span> {item.points} </span>
-            <span> {item.objectID} </span>
-            
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-//Class Button
-class Button extends Component{
-  render(){
-    const{
-      onClick,
-      className,
-      children,
-    } = this.props;
-
-    return (
-      <button
-        onClick = {onClick}
-        className = {className}
-        type = "button"
-      >
-      
-        {children}
-      </button>
-    );
-  }
-
-  
-}
 
 export default App;
